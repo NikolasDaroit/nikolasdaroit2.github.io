ac8c3c87ab2faba17d6fcd9a4e04e224dc6062b8
nikolas <nikolastnd@gmail.com>
2018-01-03 13:51:19 -0200

    html e assets

---
+++ assets/js/overlay.js
@@ -0,0 +1,11 @@
+/* Open when someone clicks on the span element */
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+
+    document.getElementById("overlaytext").text = "I already said that it will be available soon™ "
+    // document.getElementById("myNav").style.width = "0%";
+}
